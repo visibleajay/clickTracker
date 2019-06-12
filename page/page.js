3ff1d24cd59b7437a83d30107ce8c5a26ea1bb5e
Ajay Kumar <ajay@mihubb.com>
2019-06-12 16:27:00 +0530

    Remove comments from Page.js file.
    Remove unwanted code and unneeded comments from page.js file.

--- page/page.js
+++ page/page.js
@@ -20,7 +20,6 @@
 
         for ( let index in elementList ) {
             
-            console.log("element index ", index);
             const prop      =   elementList[index];
             let btnElement  =   getBtnElement(prop);
 
@@ -33,68 +32,17 @@
             let hoverBtnWidth          =   150;
             let hoverBtnHeight         =   50;
 
-            // if ( 
-            //     ( isNaN(clickedElementWidth) || isNaN(clickedElementHeight) ) || 
-            //     ( clickedElementWidth > permissibleElementWidth || clickedElementHeight > permissibleElementHeight )
-            //     ) {
-
-            //     let hoverBtnElement            =   document.createElement("button");
-                
-            //     hoverBtnElement.style.width    =   hoverBtnWidth + "px";
-            //     hoverBtnElement.style.height   =   hoverBtnHeight + "px";
-            //     hoverBtnElement.textContent    =   "Over Here";
-            //     hoverBtnElement.style.backgroundColor  =   "#d3d3d3";
-            //     hoverBtnElement.style.padding  =   "15px";
-
-            //     btnElement.style.display        =   "none";
-
-            //     btnElement.onclick              =  computeQuerySelector
-
-            //     hoverBtnElement.appendChild(btnElement);
-
-            //     hoverBtnElement.onmouseover =   () => {
-            //         btnElement.style.zIndex     =   "10";
-            //         btnElement.style.display    =   "block";
-            //         btnElement.style.position      =   "absolute";
-            //     }
-            //     hoverBtnElement.onmouseleave =   () => {
-            //         btnElement.style.display    =   "none";
-            //     }
-                
-            //     // let previousElement     =   null;
-
-            //     // if ( parentDiv.lastElementChild.style.height.length == 0 || parentDiv.lastElementChild.style.width.length == 0 ) {
-            //     //     previousElement     =   parentDiv.lastElementChild.firstElementChild;
-            //     // } else {
-            //     //     previousElement     =   parentDiv.lastElementChild;
-            //     // }
-
-            //     let [left, top] =   isFirst ? [20, 20] : computeTopLeft(parentDiv.lastElementChild, hoverBtnElement);
-            //     hoverBtnElement.style.position =   "absolute";
-            //     hoverBtnElement.style.top   =   top+"px";
-            //     hoverBtnElement.style.left  =   left+"px";
-            //     console.log("hover element Top ", top);
-            //     console.log("hover element Left ", left);
-
-            //     parentDiv.appendChild(hoverBtnElement);
-
-            // } else {
-                let [left, top] = isFirst ? [20, 20] : computeTopLeft(parentDiv.lastElementChild, btnElement.firstElementChild);
-                console.log("clicked element Top ", top);
-                console.log("clicked element Left ", left);
-                btnElement.style.position    =  "absolute";
-                btnElement.style.top        =  top+"px";
-                btnElement.style.left       =  left+"px";
-                btnElement.addEventListener("click", function ($event){
-                    computeQuerySelector($event, index);
-                });
-                parentDiv.appendChild(btnElement);
-                // console.log(prop, btnElement);
-                if ( prop.hasOwnProperty("bgColor") ) {
-                    console.log("inside element " + prop["bgColor"]);
-                    addBgColorToBtn(btnElement.firstElementChild, prop["bgColor"]);
-                }
-            // }
+            let [left, top] = isFirst ? [20, 20] : computeTopLeft(parentDiv.lastElementChild, btnElement.firstElementChild);
+            btnElement.style.position    =  "absolute";
+            btnElement.style.top        =  top+"px";
+            btnElement.style.left       =  left+"px";
+            btnElement.addEventListener("click", function ($event){
+                computeQuerySelector($event, index);
+            });
+            parentDiv.appendChild(btnElement);
+            if ( prop.hasOwnProperty("bgColor") ) {
+                addBgColorToBtn(btnElement.firstElementChild, prop["bgColor"]);
+            }
             isFirst         =   false;
         }
     }, err => {
@@ -158,33 +106,12 @@
 
         let previousElementOffset = previousElement.getBoundingClientRect();
 
-        let previousElementLeft =   previousElementOffset.left;
         let previousElementTop  =   previousElementOffset.top;
-
-        let previousElementWidth =  previousElementOffset.width;
         let previousElementHeight =  previousElementOffset.height;
 
-        let parentLeft    = 0, parentTop = 0;
-        let requiredWidth = previousElementWidth + parseFloat(currentElement.style.width) + previousElementLeft + 20;
+        let parentLeft    = 20, parentTop = previousElementTop + previousElementHeight + 20;;
         
-        // if ( requiredWidth <= 800 ) {
-        //     parentTop     = previousElementTop;
-        //     parentLeft    = previousElementLeft + previousElementWidth + 20;
-        // } else {
-            parentTop     = previousElementTop + previousElementHeight + 20;
-            parentLeft    = 20;
-        // }
-
         return [parentLeft, parentTop];
-                /*  1) Decide first element
-                    2) If first element, append with top: 20 and left as 20.
-                    3) If second element, compute last element child top,left.
-                    4) add offsetwidth and increment left by 20, check computed width + element size lesser than div width
-                    5) Increment left.
-                    6) else element width > available width, take left as 20 and top as previous element top + 20.
-                    7) Append element to body
-                    8) Redo the process for next element.
-                */
     }
 
     function getRandomColor() {
